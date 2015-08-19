8603ad04fd49ea829731f9209fc667fd99c430df
Will Fortin <fortinw4@gmail.com>
2015-08-18 20:52:22 -0700

    last changes before adding gallery.html

--- index.js
+++ index.js
@@ -2,18 +2,39 @@
 
 $(function(){
 
-	var usersPath = 'https://localhost:3000/users/'
-	var homePath = 'https://localhost:8080/'
+	$('form').submit(function(e){
+		e.preventDefault()
+		var values = {
+			id: _.random(1, 10),
+			treeId: _.random(1, 10),
+			firstName: $('.first-name').val(),
+			lastName: $('.last-name').val(),
+			email: $('.email').val(),
+			city: $('.city').val(),
+			state: $('.region-select-secondary').val(),
+			zip: $('.zip').val(),
+			country: $('.region-select-primary').val()
+		}
 
-	$('.submit').on('click', function(){
-		$.get(usersPath).done(function(users){
-			users.forEach(function(user){
-				$('body').append('TEST')
+		$.post('http://localhost:3000/users', values)
+			.done(function(){
+				console.log(values.image)
+				if (values.id === values.treeId) {
+					console.log(values.image)
+				} else {
+					console.log('matching error')
+				}
+				$('body').append('<p>' + values.firstName + '</p>')
 			})
-		})
+			.fail(function(){
+				console.log('form submission error!')
+			})
 	})
-
-
-
-
 })
+
+
+
+
+// read in all values from the form
+// put them inside object literal
+// console log object literal
