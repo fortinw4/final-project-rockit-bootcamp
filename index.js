a4b895d8659a52982eb098e3060e7a3254e92fd1
Will Fortin <fortinw4@gmail.com>
2015-08-21 17:55:13 -0700

    -remove all % except widths -clean up css -add more maps -delete node_modules

--- index.js
+++ index.js
@@ -4,15 +4,14 @@ $(function(){
 
 	var usersPath = 'http://localhost:3000/users/'
 	var treePath = 'http://localhost:3000/treeImages/'
-	var uploadedImage;
+	var uploadedImage
 
 	// Mimicking our file upload
 	$.get(treePath)
 		.done(function(treeImages){
 			var randomImageId = _.random(0, treeImages.length - 1)
-			uploadedImage = treeImages[randomImageId];
-		});
-
+			uploadedImage = treeImages[randomImageId]
+		})
 
 	$('.get-involved-form').submit(function(e){
 		e.preventDefault()
@@ -35,7 +34,7 @@ $(function(){
 				location.href = 'members.html'
 			})
 			.fail(function(){
-				console.log('form submission error!')
+				alert('form submission error!')
 			})
 
 	})
