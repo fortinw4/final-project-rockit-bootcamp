ed26269e148b15c41aa8dd428340bc1dcb22cf5b
Will Fortin <fortinw4@gmail.com>
2015-08-23 13:03:23 -0700

    -add how-to-selfie functionality -fix media queries for about.html

--- about.js
+++ about.js
@@ -3,35 +3,86 @@
 $(function(){
 
 	var paraPath = 'http://localhost:3000/howToParas/'
-	var paraDisplay = $('#template-para-tree').html()
-	var templatePara = Handlebars.compile(paraDisplay)
-	
 
-	$('.how-to-tree').on('click', function(e){
-		e.preventDefault()
+	var paraTreeDisplay = $('#template-para-tree').html()
+	var templateParaTree = Handlebars.compile(paraTreeDisplay)
+	
+	var paraSelfieDisplay = $('#template-para-selfie').html()
+	var templateParaSelfie = Handlebars.compile(paraSelfieDisplay)
+
+	var resultTree
+	var resultSelfie
 
 
 		$.get(paraPath)
 			.done(function(paras){
 				paras.forEach(function(para){
-					var result = templatePara(para)
-					console.log(para.para1)
 
-					$('.para1').append(result)
-					// $('.para2').append(result)
+					$('.how-to-tree').on('click', function(e){
+						e.preventDefault()
+						resultTree = templateParaTree(para)
+						$('.para1').append(resultTree)
+					})	
 
-				})	
+					$('.how-to-selfie').on('click', function(e){
+						e.preventDefault()
+						resultSelfie = templateParaSelfie(para)
+						$('.para1').remove()
+						$('.para2').append(resultSelfie)
+					})
+					
 			})
-		
 	})
 })
 
 
 
-$('.how-to-selfie').on('click', function(e){
-	e.preventDefault()
-	$('.para1').remove()
-	$('.para2').append(para.paraa)
-})
+
+
+
+
+
+
+
+
+
+
+
+
+
+// $(function(){
+
+// 	var paraPath = 'http://localhost:3000/howToParas/'
+// 	var paraDisplay = $('#template-para-tree').html()
+// 	var templatePara = Handlebars.compile(paraDisplay)
+	
+
+// 	$('.how-to-tree').on('click', function(e){
+// 		e.preventDefault()
+
+
+// 		$.get(paraPath)
+// 			.done(function(paras){
+// 				paras.forEach(function(para){
+// 					var result = templatePara(para)
+// 					console.log(para.para1)
+
+// 					$('.para1').append(result)
+// 					// $('.para2').append(result)
+
+// 				})	
+// 			})
+		
+// 	})
+// })
+
+
+
+// $('.how-to-selfie').on('click', function(e){
+// 	e.preventDefault()
+// 	$('.para1').remove()
+// 	$('.para2').append(para.paraa)
+// })
+
 
 
